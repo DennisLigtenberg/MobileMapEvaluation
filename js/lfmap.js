11096289d9a59f11b7b6fc8d3cc94d76a3c3a903
dligenb <dligtenb@hsr.ch>
2014-10-22 09:48:30 +0200

    Refactoring / Added Mapbox Satelite

--- js/lfmap.js
+++ js/lfmap.js
@@ -1,40 +1,70 @@
 var swissstyle = L.tileLayer('http://tile.osm.ch/osm-swiss-style/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://www.osm.ch/">osm.ch</a>',
+        attribution: 'Map data &copy; <a href="http://www.osm.ch/">osm.ch</a> | <a href="http://www.hsr.ch/geometalab">By GeometaLab</a>',
     }),
-    osm = L.tileLayer('http://tile.osm.ch/21781/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://www.osm.ch/">osm.ch</a>',
+    mapbox = L.tileLayer('http://api.tiles.mapbox.com/v4/sfkeller.k0onh2me/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2ZrZWxsZXIiLCJhIjoia3h4T3pScyJ9.MDLSUwpRpPqaV7SVfGcZDw', {
+        attribution: 'Map data &copy; <a href="http://www.mapbox.com">Mapbox</a> | <a href="http://www.hsr.ch/geometalab">By GeometaLab</a>',
     });
 
 var castleicon = L.icon({
-    iconUrl: 'Castle.png',
+    iconUrl: 'img/Castle.png',
     iconSize: [28, 28]
 });
 
-var points = new L.LayerGroup();
-L.geoJson(castles, {
+var castles = new L.LayerGroup();
+
+$.getJSON("geojson/castles.geojson", function(data) {
+    var jsoncastles = L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {icon: castleicon });
         }
-}).addTo(points);
+    });
+    jsoncastles.addTo(castles);
+});
 
-/*var restaurants = new L.LayerGroup();
-L.geoJson(restaurants).addTo(restaurants);*/
+var restaurants = new L.LayerGroup();
+
+/*$.getJSON("geojson/restaurants.geojson", function(data) {
+    var restaurants = L.geoJson(data, {
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        },
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {icon: castleicon });
+        }
+    });
+    restaurants.addTo(points);
+});*/
+
 
 var map = L.map('map', {
     center: [47.2267, 8.8167],
     zoom: 11,
     maxzoom: 11,
-    layers: [swissstyle, points]
+    layers: [swissstyle, castles]
 });
 
 var baseMaps = {
-    "OSM": osm,
+    "Mapbox": mapbox,
     "SwissStyle": swissstyle
 };
 
 var overlay = {
-	"points": points,
-	//"restaurants": restaurants
+	"Castles": castles,
+	"Restaurants": restaurants
 };
+
+var p1 = L.point(5.9700, 45.8300),
+    p2 = L.point(10.4900, 47.8100),
+    bounds = L.bounds(p1, p2);
+    
+var fitBounds = L.Control.extend({
+    onAdd: function (map) {
+        var container = L.DomUtil.create('div', 'fitBounds');
+		map.fitBounds(bounds);
+        return container;
+    }
+});
+
+map.addControl(new fitBounds());
 L.control.scale({imperial: false}).addTo(map);
 L.control.layers(baseMaps, overlay).addTo(map);
--- js/olmap.js
+++ js/olmap.js
@@ -1,76 +1,79 @@
-proj4.defs("EPSG:21781","+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs");
+proj4.defs("EPSG:21781", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs");
 
 var map = new ol.Map({
-	target: 'map',
-	interactions: ol.interaction.defaults().extend([
-    new ol.interaction.DragRotateAndZoom()
-  ]),
-	controls: ol.control.defaults({
-	    attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-	        collapsible: false
-	    })
-	}).extend([
-	    new ol.control.ZoomToExtent({
-	        extent: [
-	            599266.301756, 5718903.20002,
-	            1159384.634427, 6050241.789859
-	        ]
-	    }),
-	    new ol.control.ScaleLine(),
-	    new ol.control.Rotate({
-	    	autoHide: false
-	    })
-	]),
+    target: 'map',
+    interactions: ol.interaction.defaults().extend([
+        new ol.interaction.DragRotateAndZoom()
+    ]),
+    controls: ol.control.defaults().extend([
+        new ol.control.ZoomToExtent({
+            extent: [
+                /*599266.301756, 5718903.20002,
+                1159384.634427, 6050241.789859,*/
+                664577.360036, 5753148.32695,
+                1167741.45842, 6075303.61197
+            ]
+        }),
+        new ol.control.ScaleLine(),
+        new ol.control.Rotate({
+            autoHide: false
+        })
+    ]),
     layers: [
-    	new ol.layer.Group({
-	        'title': 'Maps',
-	        layers: [
-	            new ol.layer.Tile({
-	                title: "Swiss Style OSM",
-	                type: "base",
-	                source: new ol.source.XYZ({
-	                    url: 'http://tile.osm.ch/osm-swiss-style/{z}/{x}/{y}.png'
-	                })
-	            }),
-	            new ol.layer.Tile({
-	                title: "OSM CH",
-	                type: "base",
-	                visible: false,
-	                source: new ol.source.XYZ({
-	                    url: 'http://tile.osm.ch/21781/{z}/{x}/{y}.png',
-	                })
-	            }),
-	        ]
-	    }),
-	   new ol.layer.Group({
-	   		'title': 'Points',
-	        layers: [
-	        	new ol.layer.Vector({
-	        		title: "Castles",
-	        		source: new ol.source.GeoJSON({
-    					url:'olcastles.geojson',
- 						projection: 'EPSG:3857'
- 					}),
-					style: new ol.style.Style({
-						image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
-								anchorXUnits: 'pixels',
-								anchorYUnits: 'pixels',
-								src: "Castle.png",
-								width: "38",
-								height: "38",
-							}))
-					})
-	        	}),
-	        	/*new ol.layer.Vector({
+        new ol.layer.Group({
+            'title': 'Maps',
+            layers: [
+                new ol.layer.Tile({
+                    title: "Swiss Style OSM",
+                    type: "base",
+                    source: new ol.source.XYZ({
+                        url: 'http://tile.osm.ch/osm-swiss-style/{z}/{x}/{y}.png',
+						attributions: [
+	                        new ol.Attribution({
+	                            html: 'Map data &copy; <a href="http://www.osm.ch/">osm.ch</a></br><a href="http://www.hsr.ch/geometalab">By GeometaLab</a>'
+	                        }),
+	                    ],
+                    })
+                }),
+                new ol.layer.Tile({
+                    title: "OSM CH",
+                    type: "base",
+                    visible: false,
+                    source: new ol.source.XYZ({
+                        url: 'http://a.tiles.mapbox.com/v3/tmcw.map-j5fsp01s/{z}/{x}/{y}.png',
+                    })
+                }),
+            ]
+        }),
+        new ol.layer.Group({
+            'title': 'Points',
+            layers: [
+                new ol.layer.Vector({
+                    title: "Castles",
+                    source: new ol.source.GeoJSON({
+                        url: 'geojson/castles.geojson',
+                        projection: 'EPSG:3857'
+                    }),
+                    style: new ol.style.Style({
+                        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+                            anchorXUnits: 'pixels',
+                            anchorYUnits: 'pixels',
+                            src: "img/Castle.png",
+                            width: "38",
+                            height: "38",
+                        }))
+                    })
+                }),
+                /*new ol.layer.Vector({
 	        		title: "Restaurants",
 	        		visible: false,
 	        		source: new ol.source.GeoJSON({
-    					url:'restaurants.geojson',
+    					url:'geojson/restaurants.geojson',
  						projection: 'EPSG:3857'
  					}),
 	        	})*/
-	        ]
-	    })
+            ]
+        })
     ],
     view: new ol.View({
         center: ol.proj.transform([8.8167, 47.2267], 'EPSG:4326', 'EPSG:3857'),
@@ -80,5 +83,3 @@ var map = new ol.Map({
 
 var layerSwitcher = new ol.control.LayerSwitcher();
 map.addControl(layerSwitcher);
-
-
