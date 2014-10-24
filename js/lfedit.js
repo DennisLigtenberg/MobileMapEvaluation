89579dfdea7e3aea7f9e08dacbdcb5349e9c23a8
dligenb <dligtenb@hsr.ch>
2014-10-24 11:03:46 +0200

    Refactor

--- js/lfedit.js
+++ js/lfedit.js
@@ -1,40 +1,16 @@
-L.NewLineControl = L.Control.extend({
-
-    options: {
-        position: 'topleft'
-    },
-
-    onAdd: function (map) {
-        var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar'),
-            link = L.DomUtil.create('a', '', container);
-
-        link.href = '#';
-        link.title = 'Create a new line';
-        link.innerHTML = 'L';
-        L.DomEvent.on(link, 'click', L.DomEvent.stop)
-            .on(link, 'click', function () {
-                map.editTools.startPolyline();
-            });
-
-        return container;
-    }
-});
-
 L.NewPolygonControl = L.Control.extend({
 
     options: {
         position: 'topleft'
     },
-
-    onAdd: function (map) {
+    onAdd: function(map) {
         var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar'),
             link = L.DomUtil.create('a', '', container);
-
         link.href = '#';
         link.title = 'Create a new polygon';
         link.innerHTML = 'P';
         L.DomEvent.on(link, 'click', L.DomEvent.stop)
-            .on(link, 'click', function () {
+            .on(link, 'click', function() {
                 map.editTools.startPolygon();
             });
 
@@ -42,13 +18,31 @@ L.NewPolygonControl = L.Control.extend({
     }
 });
 
-L.NewMarkerControl = L.Control.extend({
-
+L.NewLineControl = L.Control.extend({
     options: {
         position: 'topleft'
     },
+    onAdd: function(map) {
+        var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar'),
+            link = L.DomUtil.create('a', '', container);
 
-    onAdd: function (map) {
+        link.href = '#';
+        link.title = 'Create a new line';
+        link.innerHTML = 'L';
+        L.DomEvent.on(link, 'click', L.DomEvent.stop)
+            .on(link, 'click', function() {
+                map.editTools.startPolyline();
+            });
+
+        return container;
+    }
+});
+
+L.NewMarkerControl = L.Control.extend({
+    options: {
+        position: 'topleft'
+    },
+    onAdd: function(map) {
         var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar'),
             link = L.DomUtil.create('a', '', container);
 
@@ -56,7 +50,7 @@ L.NewMarkerControl = L.Control.extend({
         link.title = 'Add a new marker';
         link.innerHTML = 'M';
         L.DomEvent.on(link, 'click', L.DomEvent.stop)
-            .on(link, 'click', function () {
+            .on(link, 'click', function() {
                 map.editTools.startMarker();
             });
 
@@ -64,7 +58,6 @@ L.NewMarkerControl = L.Control.extend({
     }
 });
 
-
-map.addControl(new L.NewMarkerControl());
 map.addControl(new L.NewLineControl());
+map.addControl(new L.NewMarkerControl());
 map.addControl(new L.NewPolygonControl());
